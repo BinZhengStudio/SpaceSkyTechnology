7423e01834f345ea2bf78d5b4252bc0613590ee6
vvvbbbcz <vvvbbbcz@163.com>
2022-05-29 16:35:01 +0800

    Fix Rider Position

--- src/main/java/cn/bzgzs/spaceplane/world/entity/BZ20PlaneEntity.java
+++ src/main/java/cn/bzgzs/spaceplane/world/entity/BZ20PlaneEntity.java
@@ -2,13 +2,26 @@ package cn.bzgzs.spaceplane.world.entity;
 
 import cn.bzgzs.spaceplane.world.item.ItemList;
 import net.minecraft.core.NonNullList;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class BZ20PlaneEntity extends BasePlaneEntity {
 	public BZ20PlaneEntity(EntityType<?> type, Level level) {
 		super(type, level);
+	}
+
+	@Override
+	public Vec3 getRiderOffset() {
+		return new Vec3(0, 0.5, 2);
+	}
+
+	@Override
+	protected float getEyeHeight(Pose pose, EntityDimensions size) {
+		return 0;
 	}
 
 	@Override
--- src/main/java/cn/bzgzs/spaceplane/world/entity/BasePlaneEntity.java
+++ src/main/java/cn/bzgzs/spaceplane/world/entity/BasePlaneEntity.java
@@ -10,17 +10,25 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * 战斗机的抽象类<br>
+ * 以下注释所述的战机坐标系，均以飞机为原点，飞机头朝向为<strong>Z轴</strong>正方向，飞机EyeHeight为Y轴原点，按右手定则建立的坐标系<br>
+ * 本类中定义所有关于角度的变量，均采用弧度制
+ */
 public abstract class BasePlaneEntity extends Entity {
 	// 发动机输出大小
 	private static final EntityDataAccessor<Float> POWER = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
-	// 各轴方向上的速度，以下变量中的X、Y、Z轴，均以飞机为原点，飞机头朝向为X轴正方向，按右手定则建立坐标系
+	// 各轴方向上的速度，以下变量中的X、Y、Z轴，均为战机坐标系中的X、Y、Z轴
 	private static final EntityDataAccessor<Float> X_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> Y_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> Z_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
@@ -28,15 +36,17 @@ public abstract class BasePlaneEntity extends Entity {
 	private static final EntityDataAccessor<Float> ACCEL_X = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> ACCEL_Y = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> ACCEL_Z = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
-	// 各轴的旋转方向，代表X轴，Y、Z轴原版已提供
+	// 各轴的旋转方向，代表X、Y、Z轴，均采用弧度制
+	private static final EntityDataAccessor<Float> PITCH = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
+	private static final EntityDataAccessor<Float> YAW = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> ROLL = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	// 各轴的旋转速度，分别代表X、Y、Z轴
-	private static final EntityDataAccessor<Float> YAW_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> PITCH_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
+	private static final EntityDataAccessor<Float> YAW_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> ROLL_SPEED = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	// 各轴的旋转加速度
-	private static final EntityDataAccessor<Float> YAW_ACCEL = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> PITCH_ACCEL = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
+	private static final EntityDataAccessor<Float> YAW_ACCEL = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	private static final EntityDataAccessor<Float> ROLL_ACCEL = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.FLOAT);
 	// 是否抛副油箱
 	private static final EntityDataAccessor<Boolean> AUXILIARY_TANK = SynchedEntityData.defineId(BasePlaneEntity.class, EntityDataSerializers.BOOLEAN);
@@ -56,7 +66,9 @@ public abstract class BasePlaneEntity extends Entity {
 		this.entityData.define(ACCEL_X, 0.0F); // X轴加速度
 		this.entityData.define(ACCEL_Y, 0.0F); // Y轴加速度
 		this.entityData.define(ACCEL_Z, 0.0F); // Z轴加速度
-		this.entityData.define(ROLL, 0.0F); // X轴旋转
+		this.entityData.define(PITCH, 0.0F); // X轴旋转
+		this.entityData.define(YAW, 0.0F); // Y轴旋转
+		this.entityData.define(ROLL, 0.0F); // Z轴旋转
 		this.entityData.define(YAW_SPEED, 0.0F); // X轴旋转速度
 		this.entityData.define(PITCH_SPEED, 0.0F); // Y轴旋转速度
 		this.entityData.define(ROLL_SPEED, 0.0F); // Z轴旋转速度
@@ -72,6 +84,16 @@ public abstract class BasePlaneEntity extends Entity {
 		return MovementEmission.SOUNDS;
 	}
 
+	/**
+	 * 因为Minecraft原版的实体Y坐标是按照实体底部的Y坐标来计算的，如果直接用getY()获取的Y坐标进行绕Z轴旋转的操作，会出现bug。<br>
+	 * 原版的这个方法是用来设置生物的眼睛高度，我们同时用于设置Y偏移。
+	 * @param pose 没用的参数
+	 * @param size 没用的参数
+	 * @return Y轴偏移
+	 */
+	@Override
+	protected abstract float getEyeHeight(Pose pose, EntityDimensions size);
+
 	@Override
 	public boolean isPickable() {
 		return !this.isRemoved();
@@ -82,9 +104,29 @@ public abstract class BasePlaneEntity extends Entity {
 		return true; // TODO 以后要自己搞碰撞检测
 	}
 
+	/**
+	 * 获取飞行员的骑乘位置偏移，以战机坐标系为基准
+	 * @return 骑乘位置偏移
+	 */
+	public abstract Vec3 getRiderOffset();
+
 	@Override
-	public double getPassengersRidingOffset() { // TODO 根据起落架的位置调整
-		return 0;
+	public void positionRider(Entity passenger) {
+		if (this.hasPassenger(passenger)) {
+			passenger.setPos(this.calculateRiderPosition());
+		}
+	}
+
+	private Vec3 calculateRiderPosition() {
+		return new Vec3(this.getX(), this.getY(), this.getZ()).add(this.calculateRiderOffset());
+	}
+
+	private Vec3 calculateRiderOffset() {
+		float pitch = this.entityData.get(PITCH); // TODO 需要转换为相对于世界的坐标系
+		double yaw = -Math.toRadians(this.getYRot()); // 转换为弧度制
+		double roll = Math.toRadians(this.getXRot()); // TODO 需要修改
+//		System.out.println(this.getRiderOffset().add(0, this.getEyeHeight(), 0).xRot(pitch).yRot((float) yaw).zRot((float) roll));
+		return this.getRiderOffset().add(0, this.getEyeHeight(), 0).xRot(pitch).yRot((float) yaw).zRot((float) roll);
 	}
 
 	@Override
@@ -94,7 +136,6 @@ public abstract class BasePlaneEntity extends Entity {
 
 	/**
 	 * 飞机炸掉之后留下的东西
-	 *
 	 * @return 留下的所有东西
 	 */
 	public abstract NonNullList<ItemStack> getDropItem();
