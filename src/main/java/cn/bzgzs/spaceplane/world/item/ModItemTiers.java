136c858ab22d8779d28a3cd0832253438a085096
vvvbbbcz <vvvbbbcz@163.com>
2022-07-09 23:07:17 +0800

    修正随风的代码

--- src/main/java/cn/bzgzs/spaceplane/world/item/ModItemTiers.java
+++ src/main/java/cn/bzgzs/spaceplane/world/item/ModItemTiers.java
@@ -8,48 +8,47 @@ import java.util.function.Supplier;
 
 public enum ModItemTiers implements Tier {
 
-    RHENIUM(5,3000,13.0F,10.0F,17,() -> {return Ingredient.of(ItemList.RHENIUM_INGOT.get());}),
-    NICKEL(4,1033,7.0F,8.0F,10,() -> {return Ingredient.of(ItemList.NICKEL_INGOT.get());});
+	RHENIUM(5, 3000, 13.0F, 10.0F, 16, () -> Ingredient.of(ItemList.RHENIUM_INGOT.get())),
+	NICKEL(2, 500, 7.0F, 7.0F, 14, () -> Ingredient.of(ItemList.NICKEL_INGOT.get()));
 
-    private final int uses;
-    private final float speed;
-    private final float attackDamageBonus;
-    private final int level;
-    private final int enchantmentvalue;
-    private final Supplier<Ingredient> repairIngredient;
+	private final int uses; // 耐久
+	private final float speed; // 攻击速度
+	private final float damage; // 攻击伤害
+	private final int level; // 挖掘等级
+	private final int enchantmentValue; // 附魔经验叠加
+	private final Supplier<Ingredient> repairIngredient; // 修理材料
 
-    ModItemTiers(int level,int uses,float speed,float attackDamageBonus,int enchantmentvalue,@Nonnull Supplier<Ingredient> repairIngredient){
-        this.level = level;
-        this.uses = uses;
-        this.speed = speed;
-        this.attackDamageBonus = attackDamageBonus;
-        this.enchantmentvalue = enchantmentvalue;
-        this.repairIngredient = repairIngredient;
-    }
+	ModItemTiers(int level, int uses, float speed, float damage, int enchantmentValue, @Nonnull Supplier<Ingredient> repairIngredient) {
+		this.level = level;
+		this.uses = uses;
+		this.speed = speed;
+		this.damage = damage;
+		this.enchantmentValue = enchantmentValue;
+		this.repairIngredient = repairIngredient;
+	}
 
-    public int getUses(){
-        return this.uses;
-    }
+	public int getUses() {
+		return this.uses;
+	}
 
-    public float getSpeed(){
-        return this.speed;
-    }
+	public float getSpeed() {
+		return this.speed;
+	}
 
-    public float getAttackDamageBonus(){
-        return this.attackDamageBonus;
-    }
+	public float getAttackDamageBonus() {
+		return this.damage;
+	}
 
-    public int getLevel(){
-        return this.level;
-    }
+	public int getLevel() {
+		return this.level;
+	}
 
-    public int getEnchantmentValue(){
-        return this.enchantmentvalue;
-    }
+	public int getEnchantmentValue() {
+		return this.enchantmentValue;
+	}
 
-    @Nonnull
-    public Ingredient getRepairIngredient(){
-        return this.repairIngredient.get();
-    }
-
+	@Nonnull
+	public Ingredient getRepairIngredient() {
+		return this.repairIngredient.get();
+	}
 }
